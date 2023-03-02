08d22e76c6e7e8d764bc7a4ce30db152904f3073
Alex Yang <alexyang@Alexs-MacBook-Pro.local>
2023-03-01 23:18:50 -0700

    cleanup

--- main.js
+++ main.js
@@ -1,25 +1,28 @@
-const { default: axios } = "axios";
+// const { default: axios } = "axios";
 
-console.log("linked");
+/* Debugging flag */
+let log = false;
+
 const form = document.forms[0];
 form.addEventListener("submit", (e) => {
     e.preventDefault();
-    console.log("submitted");
-    const data = new FormData(form);
-
-    for (const [key, value] of data) {
-        if (key == "vin") {
-            // Populate card title with the vin number
-            document.getElementById("vin").innerHTML = value;
-            console.log("sending request to API");
-            sendVinToServer(value);
-        }
-    }
+    clearUI();
+    let vin = findVinFromForm(new FormData(form));
+    if (log) console.log(vin);
+    sendVinToServer(vin);
+    // if (response) parseAttribute(response);
+    // else errorHandler();
 });
 
-let makeText = document.getElementById("make");
-let modelText = document.getElementById("model");
-let yearText = document.getElementById("year");
+function findVinFromForm(formData) {
+    for (const [key, value] of formData) {
+        if (key == "vin") {
+            // Populate card title with the vin number
+            return value;
+        }
+    }
+    errorHandler();
+}
 
 function sendVinToServer(vin) {
     const CarsXE_API_Key = "pa7a0cys6_sg5khu4pn_n2nms17zr";
@@ -27,17 +30,41 @@ function sendVinToServer(vin) {
     let request = new XMLHttpRequest();
     request.addEventListener("load", () => {
         // on load parse the response
-        let { attributes } = JSON.parse(request.response);
-        // console.log(attributes);
-        parseAttribute(attributes);
+        let response = JSON.parse(request.response);
+        // update UI
+        if (log) console.log(response);
+        if (response) parseAttribute(vin, response.attributes);
+        else errorHandler();
     });
+    if (log) console.log(request);
     request.open("GET", reqUrl, true);
     request.send();
+    return null;
 }
 
-function parseAttribute({ make, model, year }) {
+function parseAttribute(vin, { make, model, year }) {
+    // updateing UI
     // populate the card with response information
-    document.getElementById("make").innerHTML = make;
-    document.getElementById("model").innerHTML = model;
-    document.getElementById("year").innerHTML = year;
+    reconstructCard();
+    document.getElementById("vin").innerHTML = vin;
+    document.getElementById(
+        "car-desc"
+    ).innerHTML = `${make} ${model} (${year})`;
+}
+
+function errorHandler() {
+    console.log("An error has occurred");
+}
+function clearUI() {
+    document.getElementById("report").innerHTML = "";
+}
+function reconstructCard() {
+    document.getElementById("report").innerHTML = `
+    <div class="card" style="width: 18rem">
+        <div class="card-body">
+            <h5 class="card-title" id="car-desc"></h5>
+            <p class="card-text" id="vin"></p>
+            <!-- <a href="#" class="btn btn-primary">Go somewhere</a> -->
+        </div>
+    </div>`;
 }
